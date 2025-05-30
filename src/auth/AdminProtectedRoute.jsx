8b0d3d8fa40f4cb3294af5c4283fd00b402e896d
nik2168 <128375663+nik2168@users.noreply.github.com>
2025-05-30 15:29:31 +0530

    transaction working admin side

--- src/auth/AdminProtectedRoute.jsx
+++ src/auth/AdminProtectedRoute.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const AdminProtectedRoute = ({ children, user = true, redirect = "/" }) => {
-  if (user.role !== "admin" || !user) return <Navigate to={redirect}></Navigate>;
+  if (user?.role !== "admin" || !user) return <Navigate to={redirect}></Navigate>;
   return children ? children : <Outlet />;
   // outlet will replace the page according to route if children is not there. ...
 };
--- src/pages/admin/transactions/Transaction.jsx
+++ src/pages/admin/transactions/Transaction.jsx
@@ -13,26 +13,22 @@ import { useAdminOrdersDataQuery } from "../../../features/api.js";
 import { useErrors } from "../../../hooks/hook.jsx";
 import Loading from "../../../components/Loading.jsx";
 
-
 const TopSideButtons = ({ removeFilter, applyFilter, applySearch }) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
   const [filterParam, setFilterParam] = useState("");
   const [searchText, setSearchText] = useState("");
   const locationFilters = ["Paris", "London", "Canada", "Peru", "Tokyo"];
 
-       useEffect(() => {
-         dispatch(setPageTitle({ title: "Transactions" }));
-       }, []);
-
-
+  useEffect(() => {
+    dispatch(setPageTitle({ title: "Transactions" }));
+  }, []);
 
   const showFiltersAndApply = (params) => {
     applyFilter(params);
     setFilterParam(params);
   };
 
-  
   const removeAppliedFilter = () => {
     removeFilter();
     setFilterParam("");
@@ -119,60 +115,115 @@ const Transactions = () => {
 
   useErrors([{ isError, error }]);
 
-
-  return isLoading ? <Loading /> : (
+  return isLoading ? (
+    <Loading />
+  ) : (
     <>
-      <TitleCard
-        title="Recent Transactions"
-        topMargin="mt-2"
-        TopSideButtons={
-          <TopSideButtons
-            applySearch={applySearch}
-            applyFilter={applyFilter}
-            removeFilter={removeFilter}
-          />
-        }
-      >
-        {/* Team Member list in table format loaded constant */}
-        <div className="overflow-x-auto w-full">
-          <table className="table w-full">
-            <thead>
-              <tr>
-                <th>User</th>
-                <th>Product </th>
-                <th>Quantity</th>
-                <th>Amount</th>
-                <th>Transaction Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.orders?.map((l, k) => {
-                return (
-                  <tr key={k}>
-                      <td className="font-bold">{l.user.username}</td>
-                    <td>
+      <div className="max-w-6xl w-full mx-auto mt-6 relative bg-base-100">
+        <div className="w-full mx-auto p-0 md:p-4 lg:p-4 space-y-3">
+          {data?.userOrders?.map((order) => (
+            <div
+              key={order._id}
+              className="backdrop-blur-[50px] w-full rounded-xl shadow-md border border-base-300 p-6 space-y-2"
+            >
+              <div className="grid grid-cols-1 sm:grid-cols-3 gap-y-2 gap-x-2 text-base text-base-content">
+                <div>
+                  <span className="block text-sm font-medium text-base-content/70">
+                    Order ID
+                  </span>
+                  <span className="text-md font-normal">{order._id}</span>
+                </div>
+                <div>
+                  <span className="block text-sm font-medium text-base-content/70">
+                    Phone
+                  </span>
+                  <span className="text-lg">{order.phone}</span>
+                </div>
+                <div>
+                  <span className="block text-sm font-medium text-base-content/70">
+                    Address
+                  </span>
+                  <span className="text-md font-light">{order.address}</span>
+                </div>
+                <div>
+                  <span className="block text-sm font-medium text-base-content/70">
+                    NAME
+                  </span>
+                  <span className="text-lg capitalize">
+                    {order.user.username}
+                  </span>
+                </div>
+                <div>
+                  <span className="block text-sm font-medium text-base-content/70">
+                    Status
+                  </span>
+                  <span
+                    className={`badge badge-lg ${
+                      order.status === "Delivered"
+                        ? "badge-success"
+                        : "badge-warning"
+                    } mt-1`}
+                  >
+                    {order.status}
+                  </span>
+                </div>
+                <div>
+                  <span className="block text-sm font-medium text-base-content/70">
+                    Total Paid
+                  </span>
+                  <span className="text-lg font-semibold">
+                    ${order.orderTotal}{" "}
+                    <span className="text-sm font-extralight">[{order.paymentWay} payment]</span>
+                  </span>
+                </div>
+              </div>
 
-                      <div className="flex items-center space-x-3">
-                        <div className="avatar">
-                          <div className="mask mask-squircle w-12 h-12">
-                            <img src={l.productId.image.url} alt="Avatar" />
+              <div>
+                <h3 className="text-xl font-semibold text-base-content mb-3">
+                  Items in this Order
+                </h3>
+                <div className="w-full grid grid-cols-3 gap-2">
+                  {order?.products?.map((item, idx) => {
+                    const product = item.productId;
+                    return (
+                      <div
+                        key={idx}
+                        className="bg-base-200 rounded-lg p-2 shadow-lg"
+                      >
+                        <div className="flex items-center w-full">
+                          <img
+                            src={product?.image?.url}
+                            alt={product?.name}
+                            className="w-[5rem] h-[6rem] shadow-md object-cover rounded-lg border"
+                          />
+                          <div className="ml-6 flex flex-col justify-center items-start gap-1 ">
+                            <p className="font-bold text-sm  text-base-content capitalize">
+                              {product?.title}
+                            </p>
+                            <p className="text-sm   text-base-content font-extrabold">
+                              <span className="text-[0.7rem] font-extralight">
+                                a product by
+                              </span>{" "}
+                              {product?.company?.name}
+                            </p>
+                            <p className="text-sm text-base-content/70">
+                              <span className="font-bold">Quantity: </span>{" "}
+                              {item?.quantity}
+                            </p>
+                            <p className="text-sm text-base-content/70 font-bold">
+                              Price: ${item?.price}
+                            </p>
                           </div>
                         </div>
-                        <div>
-                          <div className="font-light">{l.productId.title}</div>
-                        </div>
                       </div>
-                    </td>
-                    <td>{l.quantity}</td>
-                    <td>${l.orderTotal}</td>
-                    <td>{moment(l.createdAt).format("D MMM")}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
+                    );
+                  })}
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      </TitleCard>
+      </div>
     </>
   );
 };
